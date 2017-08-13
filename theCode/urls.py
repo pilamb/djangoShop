61e3d16e595d79992176c431ea2258f59d359542
pilamb <perikopalotes@gmail.com>
2017-08-13 15:48:10 +0200

    better urls, human alike ordered

--- theCode/urls.py
+++ theCode/urls.py
@@ -13,47 +13,46 @@ from customForms import index, contact, about, authenticate, panel,\
 admin.autodiscover()
 urlpatterns = []
 urlpatterns += [
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'account/', include('clients.urls', namespace='account')),
-        url(r'shop/', include('shop.urls', namespace='shop')),
-        url(r'^events/', include('events.urls', namespace='events')),
         url(r'^$', index.page, name='index'),
-
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^events/', include('events.urls', namespace='events')),
+        # User related
+        url(r'^login/$', 'theCode.customForms.authenticate.login',
+            name='login'),
+        url(r'^logout/$', login_required(authenticate.logout),
+            name='logout'),
+        url(r'account/', include('clients.urls', namespace='account')),
+        url(r'^panelUser/', login_required(panel.page), name='panel'),
         url(r'^createUser$', 'theCode.views.create_user2.page',
             name='create_user'),
-        # only ADMIN, for panel
         url(r'^changeCredentials/$', login_required(passwordChange.page),
             name='change_password'),
+        # Products/Shop
+        url(r'shop/', include('shop.urls', namespace='shop')),
+        url(r'^products/$', ProductsListView.as_view(), name='products'),
+        url(r'^detailProduct/(?P<pk>\d+)/$', ProductDetailView.as_view(),
+            name='detail_product'),
         url(r'^createOrder2/(?P<pk>\d+)/$',
             login_required(create_concrete_order.page),
             name='create_order2'),
         # this creates an order when a product has already been chosen
-
+        # Charts about sales/expenses and products/visits. 4 Admins
         url(r'^chart_sales$',
             login_required(salesGraphics.page), name='chart_sales'),
         url(r'^chart_products$',
             login_required(productsGraphics.page), name='chart_products'),
-
-        url(r'^info$', info.page, name='info'),  #misc
-        url(r'^about$', about.page, name='about'),  #misc
-        url(r'^cookies$', cookies.page, name='cookies'),  #misc
-        url(r'^shipments$', shipments.page, name='shipments'),  #misc
-        url(r'^help$', help.page, name='help'),  #misc
-        url(r'^map$', sitemap.page, name='map'),  #misc
-        url(r'^terms$', terms.page, name='terms'),  #misc
-        url(r'^products/$', ProductsListView.as_view(), name='products'),
-        # url(r'^product2/$', Product2DetailView.as_view(), name='product2'),
-        url(r'^detailProduct/(?P<pk>\d+)/$', ProductDetailView.as_view(),
-            name='detail_product'),
-        # url(r'^products1$', ProductView.as_view(), name='product3'),
+        # miscellaneous/information
+        url(r'^info$', info.page, name='info'),
+        url(r'^about$', about.page, name='about'),
+        url(r'^cookies$', cookies.page, name='cookies'),
+        url(r'^shipments$', shipments.page, name='shipments'),
+        url(r'^help$', help.page, name='help'),
+        url(r'^map$', sitemap.page, name='map'),
+        url(r'^terms$', terms.page, name='terms'),
+        # Contact Form
         url(r'^contact$', contact.page, name='contact'),
-        url(r'^result/$', search.page, name='result'),  # search results
-
-        url(r'^panelUser/', login_required(panel.page), name='panel'),
-        url(r'^login/$', 'theCode.customForms.authenticate.login',
-            name='login'),
-        url(r'^logout/$', login_required(authenticate.logout), name='logout'),
-
+        # Search results
+        url(r'^result/$', search.page, name='result'),
 ]
 
 urlpatterns += (
