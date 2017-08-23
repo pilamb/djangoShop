b787e35d2c3a3f2f8b65e80205e912d4ae42168f
pilamb <perikopalotes@gmail.com>
2017-08-24 01:23:31 +0200

    remove an unused import

--- theCode/messages_app/models.py
+++ theCode/messages_app/models.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib import admin
 from django.core.urlresolvers import reverse
+
 from theCode.core.validators import *
 from clients. models import UserModel
 
